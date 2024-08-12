ddbee0daa7b1ccabe6f7e5edb0e1c917cb0d583d
Andrew Williams <andrewwilliams@netflix.com>
2024-08-12 09:34:17 -0500

    nvim: switch theme

--- dotfiles/nvim/lua/plugins/ui.lua
+++ dotfiles/nvim/lua/plugins/ui.lua
@@ -4,26 +4,9 @@ return {
     'RRethy/nvim-base16',
     lazy = false, -- Make its loaded during startup
     priority = 1000, -- Make sure its loaded before everything else
-    -- config = function ()
-    --   -- TODO Probably a better way to do this
-    --   vim.cmd "colorscheme base16-decaf"
-    --   -- Make the line number standout so its easier to find
-    --   vim.cmd "hi CursorLineNR guifg=yellow"
-    -- end
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
     config = function ()
-      -- XXX HACK HALP
-      -- If I load base16-decaf first, my entire editor goes to gray scale.
-      -- Presumably because of some weird treesitter changes. However, if I
-      -- first load tokyonight, then immediately switch, it works fine.
-      -- :shrug:
-      vim.cmd "colorscheme tokyonight"
-      vim.cmd "colorscheme base16-decaf"
+      -- TODO Probably a better way to do this
+      vim.cmd "colorscheme base16-eighties"
       -- Make the line number standout so its easier to find
       vim.cmd "hi CursorLineNR guifg=yellow"
     end
