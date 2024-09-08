528cb85dd611dfd70fbed6d94a1e3029ec6a671d
Andrew Williams <williams.andrew@gmail.com>
2024-09-07 22:10:41 -0500

    nix: updates from work MBP

--- home-manager/dotfiles.nix
+++ home-manager/dotfiles.nix
@@ -1,13 +1,21 @@
 {
+  pkgs,
   config,
 # lib,
   ...
 }:
 let
   repoRoot = "${config.home.homeDirectory}/dot";
+  dotfiles = "../dotfiles";
+
+  alacrittyTheme = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/aarowill/base16-alacritty/c9e824811eed70d7cdb1b31614b81c2f82bf16f8/colors/base16-eighties.toml";
+    hash = "sha256-JuTdsi5VLA2HM5RH7+E/I/QCC7BlR1s4H0jGEYsAG00=";
+  };
 in
 {
   xdg.configFile."alacritty/alacritty.toml".source = ../dotfiles/alacritty/alacritty.toml;
+  xdg.configFile."alacritty/base16-eighties.toml".source = alacrittyTheme;
 
   home.file = {
     ".ssh/config".source = ../dotfiles/ssh/config;
@@ -26,4 +34,9 @@ in
   # Symlink directly to the nvim directory in our repo vs into a nix
   # derivation in the nix store.
   home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${repoRoot}/dotfiles/nvim";
+
+  # Shell scripts
+  home.file."bin/git-cowt".source = config.lib.file.mkOutOfStoreSymlink "${repoRoot}/bin/git-cowt";
+  home.file."bin/git-wip".source = config.lib.file.mkOutOfStoreSymlink "${repoRoot}/bin/git-wip";
+  home.file."bin/git-fx".source = config.lib.file.mkOutOfStoreSymlink "${repoRoot}/bin/git-fx";
 }
