798b402a6b3fc0cd8f14a4acb3ee583cce11944f
Andrew Williams <williams.andrew@gmail.com>
2025-03-18 13:21:26 -0500

    nix-darwin: remove nix-daemon enable due to error

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -33,8 +33,6 @@
     };
   };
 
-  services.nix-daemon.enable = true;
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
