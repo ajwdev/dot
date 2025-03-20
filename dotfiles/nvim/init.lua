b5f987653b47cb94e821bdf0d6a18cafd32d0d29
Andrew Williams <williams.andrew@gmail.com>
2025-03-20 12:31:44 -0500

    nvim: match Lazy docs and update lock

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -11,6 +11,22 @@
 -- automatically sourced by Neovim.
 --]]
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -18,19 +34,6 @@ vim.g.maplocalleader = " "
 -- for the Lua configs. Useful when there is an error that is going to need
 -- to be debugged.
 vim.keymap.set("n", "<leader>D", "<cmd>tabnew | tcd ~/.config/nvim | e init.lua<CR>", {silent=true})
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
   performance = {
