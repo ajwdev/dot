164849f1e2462117fab8b6d7043af61384664dcf
Andrew Williams <williams.andrew@gmail.com>
2025-03-22 22:27:18 -0500

    nvim: drop dressing.nvim
    Drop dressing.nvim plugin as its now deprecated. Replace with Snacks UI
    and Telescope.

--- dotfiles/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make'
       },
+      'nvim-telescope/telescope-ui-select.nvim',
     },
     config = function()
       local open_with_trouble = require("trouble.sources.telescope").open
@@ -47,6 +48,7 @@ return {
       }
 
 
+      require('telescope').load_extension('ui-select')
       -- To get fzf loaded and working with telescope, you need to call
       -- load_extension, somewhere after setup function:
       require('telescope').load_extension('fzf')
