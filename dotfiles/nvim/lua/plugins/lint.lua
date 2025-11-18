2a573989221afe04fd33a3ccb3743c6b3e50667a
Andrew Williams <williams.andrew@gmail.com>
2025-11-18 10:05:45 -0600

    nvim: temporarily disable golanglintci

--- dotfiles/nvim/lua/plugins/lint.lua
+++ dotfiles/nvim/lua/plugins/lint.lua
@@ -7,7 +7,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        go = { 'golangcilint' },
+        -- go = { 'golangcilint' },
       }
 
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
