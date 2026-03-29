61ce9b2b0053d6cf64b2be94abb7621d95b7fdfc
Andrew Williams <williams.andrew@gmail.com>
2026-03-29 13:47:42 -0500

    nix: fix flake registry overrides
    - Remove manual nix.registry and nixPath overrides in common.nix; NixOS
      now handles nixpkgs registry pinning automatically per-machine, and the
      old lib.mapAttrs pattern conflicted with it on glados01 (stable vs unstable)
    - Remove stale packages argument from ajwlive nixosSystem call
    - Fix ajwlive ISO imports to use modulesPath instead of angle brackets

--- nixos/ajwlive/configuration.nix
+++ nixos/ajwlive/configuration.nix
@@ -4,16 +4,17 @@
   config,
   pkgs,
   lib,
+  modulesPath,
   ...
 }:
 
 {
   imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
 
     # Provide an initial copy of the NixOS channel so that the user
     # doesn't need to run "nix-channel --update" first.
-    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
+    "${modulesPath}/installer/cd-dvd/channel.nix"
   ];
 
   nix = {
